c13f7e81ecd3266ac48634ef7593c2e6bda6f796
Lucas Giusti <giusti.lucas@gmail.com>
2024-06-04 20:09:17 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('api/doc', app, document);
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
