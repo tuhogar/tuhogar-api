590130cf85dda9d0c62629b524ac9ae9cf6ab2e3
Lucas Giusti <giusti.lucas@gmail.com>
2024-07-19 22:54:03 -0300

    add cloudinary upload

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,19 @@ import * as bodyParser from 'body-parser';
 import { CustomExceptionFilter } from './filters/custom-exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  console.log('MONGODB_URL:', process.env.MONGODB_URL);
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  console.log('MONGODB_URL (ConfigService):', configService.get<string>('MONGODB_URL'));
+  console.log('MONGODB_URL (process.env):', process.env.MONGODB_URL);
+
+  if (!configService.get<string>('MONGODB_URL')) {
+    throw new Error('Missing MONGODB_URL environment variable');
+  }
+
   // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('Tuhogar API')
