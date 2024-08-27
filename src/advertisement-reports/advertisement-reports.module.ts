107e586112ee2be7ac358361f0f8811799b2cf25
Lucas Giusti <giusti.lucas@gmail.com>
2024-08-26 22:51:59 -0300

    add get advertisement-reports endpoint

--- src/advertisement-reports/advertisement-reports.module.ts
+++ src/advertisement-reports/advertisement-reports.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FirebaseAdmin } from 'src/config/firebase.setup';
 import { AdvertisementReasonsModule } from 'src/advertisement-reasons/advertisement-reasons.module';
 import { AdvertisementReportSchema } from './interfaces/advertisement-reports.schema';
+import { AdvertisementsModule } from 'src/advertisements/advertisements.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'AdvertisementReport', schema: AdvertisementReportSchema }]),
     AdvertisementReasonsModule,
+    AdvertisementsModule,
   ],
   controllers: [AdvertisementReportsController],
   providers: [AdvertisementReportsService, FirebaseAdmin]
