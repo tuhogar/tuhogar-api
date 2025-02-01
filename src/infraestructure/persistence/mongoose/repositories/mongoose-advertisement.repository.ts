cdbe0b3c57f43c6b952f63dcb0c777370c584159
Lucas Giusti <giusti.lucas@gmail.com>
2025-02-01 14:40:11 -0300

    test latencia

--- src/infraestructure/persistence/mongoose/repositories/mongoose-advertisement.repository.ts
+++ src/infraestructure/persistence/mongoose/repositories/mongoose-advertisement.repository.ts
@@ -259,7 +259,9 @@ export class MongooseAdvertisementRepository implements IAdvertisementRepository
 
         if (accountId) filter.accountId = accountId;
 
+        console.time('-------base');
         const query = await this.advertisementModel.find(filter).populate('amenities').populate('communityAmenities').exec();
+        console.timeEnd('-------base');
         return query.map((item) => MongooseAdvertisementMapper.toDomain(item));
     }
 
