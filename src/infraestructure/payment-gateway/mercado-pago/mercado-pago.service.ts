e1028924888766447afd059fd4507f824351a4d8
Lucas Giusti <giusti.lucas@gmail.com>
2025-03-26 14:53:50 -0300

    clean code

--- src/infraestructure/payment-gateway/mercado-pago/mercado-pago.service.ts
+++ src/infraestructure/payment-gateway/mercado-pago/mercado-pago.service.ts
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IPaymentGateway } from 'src/application/interfaces/payment-gateway/payment-gateway.interface';
