903bb8727ab970133d7b372394c2c2c68c635864
Lucas Giusti <giusti.lucas@gmail.com>
2025-06-17 22:08:51 -0300

    change advertisement-statistics endpoints

--- src/application/interfaces/repositories/account-advertisement-statistics.repository.interface.ts
+++ src/application/interfaces/repositories/account-advertisement-statistics.repository.interface.ts
@@ -3,6 +3,6 @@ import { AccountAdvertisementStatistics } from 'src/domain/entities/account-adve
 export abstract class IAccountAdvertisementStatisticsRepository {
   abstract create(statistics: AccountAdvertisementStatistics): Promise<AccountAdvertisementStatistics>;
   abstract findByAccountIdAndMonth(accountId: string, month: string): Promise<AccountAdvertisementStatistics>;
-  abstract findAllByAccountId(accountId: string): Promise<AccountAdvertisementStatistics[]>;
+  abstract findAllMonthsByAccountId(accountId: string): Promise<string[]>;
   abstract update(id: string, statistics: Partial<AccountAdvertisementStatistics>): Promise<AccountAdvertisementStatistics>;
 }
